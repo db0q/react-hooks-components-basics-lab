7c721bd29798a99f1dcd452c91f13bad5ec73971
theMstafa <mm22180481@gmail.com>
2022-11-29 14:11:07 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 function NavBar() {
-  return (
+  return (<>
     <nav>
       <a href="#home">I'm a link!</a>
+      <Home />
     </nav>
+    
+    </>
   );
 }
 
@@ -17,14 +20,20 @@ function Home() {
 }
 
 {/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+    <h1>about</h1>
+  </div>
+  )
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
